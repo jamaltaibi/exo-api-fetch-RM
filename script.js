d505e4913006934fb764082e0e0d47bc9c239b9e
Your Name <jamaltaibi@MacBook-Pro-de-Jamal.local>
2023-11-30 13:26:42 +0100

    exo api rick et morty

--- script.js
+++ script.js
@@ -1,120 +1,139 @@
-const Personnage = document.querySelector('.perso')
-const Lieux = document.querySelector('.lieu')
-const Episodes = document.querySelector('.epis')
+const Personnage = document.querySelector('.perso');
+const Lieux = document.querySelector('.lieu');
+const Episodes = document.querySelector('.epis');
 
-const carteperso = document.createElement('div')
-const cartelieux = document.createElement('div')
-const carteepisodes = document.createElement('div')
+const carteperso = document.createElement('div');
+const cartelieux = document.createElement('div');
+const carteepisodes = document.createElement('div');
 
 
-carteperso.className = "lesPersonnages"
-cartelieux.className = "lesLieux"
-carteepisodes.className = "lesEpisodes"
+carteperso.className = "lesPersonnages" ; 
+cartelieux.className = "lesLieux" ; 
+carteepisodes.className = "lesEpisodes" ; 
+
+const nbrpageperso = 42 ;
+const nbrpagelocation = 7 ;
+const nbrpageepi = 3 ;
 
 
 Personnage.addEventListener ('click', () => {
-    fetch("https://rickandmortyapi.com/api/character")
-    .then(response => response.json())
-    .then(data => {
+        
+    for (let i= 1; i <= nbrpageperso; i++) {
+        fetch(`https://rickandmortyapi.com/api/character?page=${i}`)
+        .then(response => response.json())
+        .then(data => {
 
-        console.log(data.info);
+            console.log(data.info);
+            carteperso.style.display = "flex"
+            cartelieux.style.display = "none"
+            carteepisodes.style.display = "none"
+    
+            data.results.forEach(element => {
+                const cartepers = document.createElement('div')
+                cartepers.className = "lesPersonnage"
 
-        carteperso.style.display = "flex"
-        cartelieux.style.display = "none"
-        carteepisodes.style.display = "none"
+                const creernom = document.createElement('h2')
+                creernom.textContent = element.name
 
-        for (let i=0; i <= 19 ;i++) {
+                const creerimage = document.createElement('img')
+                creerimage.src = element.image
 
-            const cartepers = document.createElement('div')
-            cartepers.className = "lesPersonnage"
-
-            const creernom = document.createElement('p')
-            creernom.textContent = data.results[i].name
-
-            const creerimage = document.createElement('img')
-            creerimage.src = data.results[i].image
-
-            document.body.appendChild(carteperso)
-            carteperso.appendChild(cartepers)
-            cartepers.appendChild(creernom)
-            cartepers.appendChild(creerimage)
-        }
-    })
-})
+                document.body.appendChild(carteperso)
+                carteperso.appendChild(cartepers)
+                cartepers.appendChild(creernom)
+                cartepers.appendChild(creerimage)
+            });
+        });
+    };
+});
 
 Lieux.addEventListener('click', () => {
-    fetch('https://rickandmortyapi.com/api/location')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
 
+    for (let i= 1; i <= nbrpagelocation; i++) {
+        fetch("https://rickandmortyapi.com/api/location?page="+i)
+        .then(response => response.json())
+        .then(data => {
+        
         carteperso.style.display = "none"
         cartelieux.style.display = "flex"
         carteepisodes.style.display = "none"
 
-        for (let i=0; i <= 19 ;i++){
+            data.results.forEach(element => {
 
-            const cartelieu = document.createElement('div')
-            cartelieu.className = "leLieu"
+                const cartelieu = document.createElement('div')
+                cartelieu.className = "leLieu"
 
-            const creernom = document.createElement('p')
-            creernom.textContent = data.results[i].name
+                const creernom = document.createElement('h1')
+                creernom.textContent = element.name
 
-            const creerdimension = document.createElement('p')
-            creerdimension.textContent = data.results[i].dimension
+                const creerdimension = document.createElement('h2')
+                creerdimension.textContent = element.dimension
 
-            // const creerresidents = document.createElement ('p')
-            // creerresidents.textContent = data.results[i].residents
+                const resident = document.createElement('div')
+                resident.className = "residents"
 
-            document.body.appendChild(cartelieux)
-            cartelieux.appendChild(cartelieu)
-            cartelieu.appendChild(creernom)
-            cartelieu.appendChild(creerdimension)
-            // cartelieu.appendChild(creerresidents)
-        
-        }
-    })
+                    element.residents.forEach(element2 => {
+                        fetch(element2)
+                        .then(response => response.json())
+                        .then(data => {
+                        
+                        const creerresidents = document.createElement ('p')
+                        creerresidents.textContent = data.name
+
+                        document.body.appendChild(cartelieux)
+                        cartelieux.appendChild(cartelieu)
+                        cartelieu.appendChild(resident)
+                        resident.appendChild(creerresidents)
+                    })  
+                document.body.appendChild(cartelieux)
+                cartelieux.appendChild(cartelieu)
+                cartelieu.appendChild(creernom)
+                cartelieu.appendChild(creerdimension)
+                cartelieu.appendChild(resident)
+
+                });
+            })
+        })
+    }
 })
 
 Episodes.addEventListener('click', () => {
-    fetch('https://rickandmortyapi.com/api/episode')
-    .then(response => response.json())
-    .then(data => {
+    
+    for (let i= 1; i <= nbrpageepi; i++) {
         
-        console.log(data)
-
-        carteperso.style.display = "none"
-        cartelieux.style.display = "none"
-        carteepisodes.style.display = "flex"
-
-        for (let i=0; i <= 19 ;i++){
-
-            const carteepisode = document.createElement('div')
-            carteepisode.className = "lEpisode"
-
-            const numepi = document.createElement('p')
-            numepi.textContent = data.results[i].episode
-
-            const creernom = document.createElement('p')
-            creernom.textContent = data.results[i].name
-
-            const creerdatedesortie = document.createElement('p')
-            creerdatedesortie.textContent = data.results[i].air_date
+        fetch("https://rickandmortyapi.com/api/episode?page="+i)
+        .then(response => response.json())
+        .then(data => {
         
-            document.body.appendChild(carteepisodes)
-            carteepisodes.appendChild(carteepisode)
-            carteepisode.appendChild(numepi)
-            carteepisode.appendChild(creernom)
-            carteepisode.appendChild(creerdatedesortie)
-        }
-    })
+            console.log(data.results)
+
+            carteperso.style.display = "none"
+            cartelieux.style.display = "none"
+            carteepisodes.style.display = "flex"
+
+            data.results.forEach(element => {
+
+                const carteepisode = document.createElement('div')
+                carteepisode.className = "lEpisode"
+
+                const numepi = document.createElement('p')
+                numepi.textContent = element.episode
+
+                const creernom = document.createElement('p')
+                creernom.textContent = element.name
+
+                const creerdatedesortie = document.createElement('p')
+                creerdatedesortie.textContent = element.air_date
+        
+                document.body.appendChild(carteepisodes)
+                carteepisodes.appendChild(carteepisode)
+                carteepisode.appendChild(numepi)
+                carteepisode.appendChild(creernom)
+                carteepisode.appendChild(creerdatedesortie)
+            })
+        })
+    }
 })
-
-// fetch('https://rickandmortyapi.com/api/location')
-// .then(response => response.json())
-// .then(data => {
-//      console.log(data.results[0])
-// });
 
 
 
